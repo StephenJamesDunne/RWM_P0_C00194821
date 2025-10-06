0a08fd6e06cac2709d3a78556fac5d36ab40260e
StephenJamesDunne <C00194821@setu.ie>
2025-10-06 10:50:26 +0100

    step 1

---
+++ Sprint 6/e2e/heading.test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('should display Progress Checklist title on checklist page', async ({ page }) => {
+    await page.goto('/lab/checklist');
+    await expect(page.getByTestId('title')).toContainText('Progress Checklist');
+});
