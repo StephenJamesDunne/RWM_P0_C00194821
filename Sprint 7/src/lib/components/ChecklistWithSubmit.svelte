b5b84175c4f1a2b2dac0d9d041056ed2881d96d5
Jake Fitzgerald <115044918+Jake-Fitzgerald@users.noreply.github.com>
2025-10-15 19:32:39 +0100

    Updated text string for Jake's Branch

--- Sprint 7/src/lib/components/ChecklistWithSubmit.svelte
+++ Sprint 7/src/lib/components/ChecklistWithSubmit.svelte
@@ -99,7 +99,7 @@
 </div>
 
 {#if submittedPercentage >= targetPercent}
-    <div class="success-message">Goals reached!</div>
+    <div class="success-message">I'm blue dah bah dee bah dah die!</div>
 {/if}
 
 <button on:click={handleSubmit} class="submit-button">
